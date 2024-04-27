47b410f8f59e6ed433c513c3f6b43a668cd799ae
oghene-ella <oghenekaro57@gmail.com>
2024-04-27 10:58:54 -0500

    pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,13 @@ import { UsersService } from '../services/users.service';
 import { User, Users } from '../../types';
 import { UserComponent } from '../components/user/user.component';
 import { CommonModule } from '@angular/common';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserComponent, CommonModule],
+  imports: [UserComponent, CommonModule, MatPaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -18,11 +20,26 @@ export class HomeComponent {
   ) {}
 
   users: User[] = [];
+  totalRecords:number = 0
+  per_page: number = 6
+  page: number = 1
+
+  onPageChange(pageEvent: PageEvent) {
+    this.fetchUsers(pageEvent.pageIndex, pageEvent.pageSize)
+    console.log(this.fetchUsers(pageEvent.pageIndex, pageEvent.pageSize))
+  }
 
   ngOnInit() {
-    this.userService.getUsers("https://reqres.in/api/users", {page: 0, per_page: 6}).subscribe((users: Users) => {
-      console.log(users.data);
+    this.fetchUsers(this.page, this.per_page)
+    console.log(this.fetchUsers(this.page, this.per_page))
+  }
+
+  fetchUsers(page: number, per_page: number) {
+    this.userService.getUsers("https://reqres.in/api/users", {page, per_page})
+    .subscribe((users: Users) => {
       this.users = users.data
+      this.totalRecords = users.total
     })
   }
+
 }
